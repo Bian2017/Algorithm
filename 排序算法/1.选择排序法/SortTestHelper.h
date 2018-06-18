76286b8d64ca1d28ce5a7e614088bd01f63a2b69
wb-bgl354174 <wb-bgl354174@alibaba-inc.com>
2018-06-18 23:17:30 +0800

    测试算法的性能

--- 排序算法/1.选择排序法/SortTestHelper.h
+++ 排序算法/1.选择排序法/SortTestHelper.h
@@ -10,8 +10,7 @@ using namespace std;
 
 namespace SortTestHelper {
   // 生成有n个元素的随机数组，每个元素的随机范围为[rangeL, rangeR]
-  int *generateRandomArray(int n, int rangeL, int rangeR)
-  {
+  int *generateRandomArray(int n, int rangeL, int rangeR) {
     assert(rangeL <= rangeR);
     int *arr = new int[n];
 
@@ -26,8 +25,7 @@ namespace SortTestHelper {
   }
 
   template <typename T>
-  void printArray(T arr[], int n)
-  {
+  void printArray(T arr[], int n) {
     for (int i = 0; i < n; i++)
     {
       cout << arr[i] << " ";
@@ -37,5 +35,28 @@ namespace SortTestHelper {
     return;
   }
 
-} 
+  template <typename T>
+  bool isSorted(T arr[], int n) { // 测试排序算法是否成功
+
+    for (int i = 0; i < n - 1; i++)
+      if (arr[i] > arr[i + 1])
+        return false;
+
+    return true;
+  }
+
+  template <typename T>
+  void testSort(string sortName, void (*sort)(T[], int), T arr[], int n) { //传递函数指针
+    clock_t startTime = clock();
+    sort(arr, n);
+    clock_t endTime = clock();
+
+    assert(isSorted(arr, n));
+    
+    // 时间差值为时钟周期，所以需要除以(1s中有多少个时钟周期)
+    cout << sortName << ":" << double(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;
+
+    return;
+  }
+} // namespace SortTestHelper
 #endif
