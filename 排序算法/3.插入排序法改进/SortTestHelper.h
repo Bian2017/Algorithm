9c47043f475373f7f68e2960d31d7472035a37ee
wb-bgl354174 <wb-bgl354174@alibaba-inc.com>
2018-06-19 11:54:10 +0800

    feat:插入排序算法改进---交换操作变成赋值操作

---
+++ 排序算法/3.插入排序法改进/SortTestHelper.h
@@ -0,0 +1,88 @@
+
+#ifndef SELECTONSORT_SORTTESTHELPER_H
+#define SELECTONSORT_SORTTESTHELPER_H
+
+#include <iostream>
+#include <ctime>
+#include <cassert>
+
+using namespace std;
+
+namespace SortTestHelper {
+  // 生成有n个元素的随机数组，每个元素的随机范围为[rangeL, rangeR]
+  int *generateRandomArray(int n, int rangeL, int rangeR)
+  {
+    assert(rangeL <= rangeR);
+    int *arr = new int[n];
+
+    srand(time(NULL)); //设置随机种子，将当前的时间作为种子
+
+    for (int i = 0; i < n; i++)
+    {
+      arr[i] = rand() % (rangeR - rangeL + 1) + rangeL;
+    }
+
+    return arr;
+  }
+
+  // 生成近乎有序的随机数组
+  int *generateNearlyOrderedArray(int n, int swapTimes) {
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)     //先生成有序的数组
+      arr[i] = i;
+
+    srand(time(NULL));
+
+    for (int i = 0; i < swapTimes; i++)
+    {
+      int posx = rand() % n;
+      int posy = rand() % n;
+      swap(arr[posx], arr[posy]);
+    }
+    
+    return arr;
+  }
+
+  template <typename T>
+  void printArray(T arr[], int n) {
+    for (int i = 0; i < n; i++)
+    {
+      cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return;
+  }
+
+  template <typename T>
+  bool isSorted(T arr[], int n) { // 测试排序算法是否成功
+
+    for (int i = 0; i < n - 1; i++)
+      if (arr[i] > arr[i + 1])
+        return false;
+
+    return true;
+  }
+
+  template <typename T>
+  void testSort(string sortName, void (*sort)(T[], int), T arr[], int n) { //传递函数指针
+    clock_t startTime = clock();
+    sort(arr, n);
+    clock_t endTime = clock();
+
+    assert(isSorted(arr, n));
+
+    // 时间差值为时钟周期，所以需要除以(1s中有多少个时钟周期)
+    cout << sortName << ":" << double(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;
+
+    return;
+  }
+
+  int *copyIntArray(int a[], int n) {
+    int *arr = new int[n];
+
+    copy(a, a + n, arr);
+    return arr;
+  }
+} // namespace SortTestHelper
+#endif
--- 排序算法/3.插入排序法改进/main.cpp
+++ 排序算法/3.插入排序法改进/main.cpp
@@ -13,23 +13,27 @@ void insertionSort(T arr[], int n)
   for (int i = 1; i < n; i++)
   {
     // 寻找元素arr[i]合适的插入位置
-    for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--)
+    T e = arr[i];
+    int j;            //j保存元素e应该插入的位置
+    for (j = i; j > 0 && arr[j - 1] > e; j--)
     {
-      swap(arr[j], arr[j - 1]);
+      arr[j] = arr[j - 1];            //改进：将交换操作改成赋值操作。一次交换操作，相当于三次赋值。
     }
+    arr[j] = e;
   }
 }
 
 int main()
 {
   int n = 20000;
-  int *arr = SortTestHelper::generateRandomArray(n, 0, n);
+  // int *arr = SortTestHelper::generateRandomArray(n, 0, n);
+  int *arr = SortTestHelper::generateNearlyOrderedArray(n, 100);    // 对于近似有序的数组，插入排序的性能要远远优于选择排序
   int *arr2 = SortTestHelper::copyIntArray(arr, n);
 
   SortTestHelper::testSort("Insertion Sort", insertionSort, arr, n);
   SortTestHelper::testSort("Selection Sort", selectionSort, arr2, n);
 
-  delete[] arr;   //将空间释放出来
-  delete[] arr2; 
+  delete[] arr;     //将空间释放出来
+  delete[] arr2;
   return 0;
 }
